ffb253692563e6995b108a6761e266e8e4e43dcd
Yaren Temizel <yarentmzl@gmail.com>
2024-05-05 10:39:38 +0300

    yeni

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <img src={logo} className="App-logo" alt="logo/">
+          <p>
+            Edit <code> src/App.js </code> and save to reload.
+          </p>
+          <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
+          target="blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          
+          ></a>
     </div>
   );
 }
